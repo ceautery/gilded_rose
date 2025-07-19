1e0fe8b3733e545e37c16564cff2b49858a0cef7
ceautery <ceautery@gmail.com>
2025-07-19 09:12:06 -0400

    Spec refactor to look at change by

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -5,25 +5,41 @@ require 'rspec'
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
+  subject(:gr) { GildedRose.new(items) }
+
   context 'with a simple item' do
-    subject(:gr) { GildedRose.new([item]) }
+    let(:items) { [item] }
 
-    let :item do
-      Item.new('foo', 0, 0)
+    describe 'a zero quality item at sell date' do
+      let(:item) do
+        Item.new('foo', 0, 0)
+      end
+
+      it 'does not change the name' do
+        expect { gr.update_quality }.not_to(change { item.name })
+      end
+
+      it 'quality is never negative' do
+        expect { gr.update_quality }.not_to(change { item.quality })
+      end
+
+      it 'sell in decrements' do
+        expect { gr.update_quality }.to change { item.sell_in }.by(-1)
+      end
     end
 
-    before { gr.update_quality }
+    describe 'a one quality item before sell date' do
+      let(:item) do
+        Item.new('foo', 1, 1)
+      end
 
-    it 'does not change the name' do
-      expect(item.name).to eq('foo')
-    end
+      it 'quality decrements' do
+        expect { gr.update_quality }.to change { item.quality }.by(-1)
+      end
 
-    it 'quality is never negative' do
-      expect(item.quality).to eq(0)
-    end
-
-    it 'sell in decrements' do
-      expect(item.sell_in).to eq(-1)
+      it 'sell in decrements' do
+        expect { gr.update_quality }.to change { item.sell_in }.by(-1)
+      end
     end
   end
 end
