abdb466b34abca9b3abb2d00a7cafc5a83459f60
ceautery <ceautery@gmail.com>
2025-07-19 10:16:52 -0400

    update_quality refactor, passes rubocop

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,29 +6,56 @@ class GildedRose
     @items = items
   end
 
+  def legendary?(item)
+    item.name == 'Sulfuras, Hand of Ragnaros'
+  end
+
+  def appreciates?(item)
+    item.name == 'Aged Brie' || backstage?(item)
+  end
+
+  def backstage?(item)
+    item.name == 'Backstage passes to a TAFKAL80ETC concert'
+  end
+
+  def decrement_item_quality(item)
+    item.quality = [item.quality - 1, 0].max
+  end
+
+  def increment_item_quality(item)
+    qual = item.quality + 1
+    if backstage?(item)
+      qual += 1 if item.sell_in < 11
+      qual += 1 if item.sell_in < 6
+    end
+    item.quality = [qual, 50].min
+  end
+
+  def past_date_adjustment(item)
+    if item.name != 'Aged Brie'
+      if !backstage?(item)
+        decrement_item_quality(item)
+      else
+        item.quality = 0
+      end
+    elsif item.quality < 50
+      item.quality = item.quality + 1
+    end
+  end
+
   def update_quality
     @items.each do |item|
-      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
-      elsif item.quality < 50
-        item.quality = item.quality + 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          item.quality = item.quality + 1 if (item.sell_in < 11) && (item.quality < 50)
-          item.quality = item.quality + 1 if (item.sell_in < 6) && (item.quality < 50)
-        end
+      next if legendary?(item)
+
+      if appreciates?(item)
+        increment_item_quality(item)
+      else
+        decrement_item_quality(item)
       end
-      item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
-      if item.sell_in.negative?
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
-          else
-            item.quality = item.quality - item.quality
-          end
-        elsif item.quality < 50
-          item.quality = item.quality + 1
-        end
-      end
+
+      item.sell_in = item.sell_in - 1
+
+      past_date_adjustment(item) if item.sell_in.negative?
     end
   end
 end
