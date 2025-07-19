2e3174bb2a355c8c8cb035138d90dedc667c21f1
ceautery <ceautery@gmail.com>
2025-07-19 08:30:20 -0400

    Basic spec for single non-special item

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,11 +1,29 @@
+# frozen_string_literal: true
+
 require 'rspec'
 
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-  it "does not change the name" do
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    expect(items[0].name).to eq "foo"
+  context 'with a simple item' do
+    subject(:gr) { GildedRose.new([item]) }
+
+    let :item do
+      Item.new('foo', 0, 0)
+    end
+
+    before { gr.update_quality }
+
+    it 'does not change the name' do
+      expect(item.name).to eq('foo')
+    end
+
+    it 'quality is never negative' do
+      expect(item.quality).to eq(0)
+    end
+
+    it 'sell in decrements' do
+      expect(item.sell_in).to eq(-1)
+    end
   end
 end
